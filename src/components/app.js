ac00191de7b0d5750a0540e811ab125ea77f50d5
Anderson Tsang <andyderson@gmail.com>
2018-02-02 13:42:08 -0800

    Basic Windows

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,25 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  templateUrl: 'src/templates/app.html'
 });
+
+// var myMod = angular.module('myMod', ['ngRoute']);
+// myMod.component('home', {
+//   template: '<h1>Home</h1><p>Hello, {{ $ctrl.user.name }} !</p>',
+//   controller: function() {
+//     this.user = {name: 'world'};
+//   }
+// });
+// myMod.config(function($routeProvider) {
+//   $routeProvider.when('/', {
+//     template: '<home></home>'
+//   });
+// });
+
+// angular.module('heroApp').component('heroDetail', {
+//   templateUrl: 'heroDetail.html',
+//   bindings: {
+//     hero: '='
+//   }
+// });
